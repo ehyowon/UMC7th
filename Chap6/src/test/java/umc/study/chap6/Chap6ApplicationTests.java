00195532c21df37d887745bc628cb3ba1bb57a76
이효원 <129304827+ehyowon@users.noreply.github.com>
2024-11-19 14:02:38 +0900

    Add files via upload

---
+++ Chap6/src/test/java/umc/study/chap6/Chap6ApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.study.chap6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
